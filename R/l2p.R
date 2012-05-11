8baa3cef3b90e2fba6dfa54cf89a6100ed8a07bb
Dan Kelley <kelley.dan@gmail.com>
2012-05-11 15:57:15 -0300

    update grading scheme

--- R/l2p.R
+++ R/l2p.R
@@ -1,21 +1,19 @@
-l2p <- function(l, scale=4.3)
+l2p <- function(l, scale=100)
 {
     rval <- NULL
     for (letter in l) {
         if (scale == 100) {                 # BUG: should permit scheme; should base on p2l to be DRY
-            if (length(grep("^A\\+$", letter, ignore.case=TRUE))>0) rval <- c(rval, 97)
-            else if (length(grep("^A$",    letter, ignore.case=TRUE))>0) rval <- c(rval, 92)
-            else if (length(grep("^A\\-$", letter, ignore.case=TRUE))>0) rval <- c(rval, 85)
-            else if (length(grep("^B\\+$", letter, ignore.case=TRUE))>0) rval <- c(rval, 79)
-            else if (length(grep("^B$",    letter, ignore.case=TRUE))>0) rval <- c(rval, 74)
-            else if (length(grep("^B\\-$", letter, ignore.case=TRUE))>0) rval <- c(rval, 69)
-            else if (length(grep("^C\\+$", letter, ignore.case=TRUE))>0) rval <- c(rval, 65)
-            else if (length(grep("^C$",    letter, ignore.case=TRUE))>0) rval <- c(rval, 61)
-            else if (length(grep("^C\\-$", letter, ignore.case=TRUE))>0) rval <- c(rval, 57)
-            else if (length(grep("^D\\+$", letter, ignore.case=TRUE))>0) rval <- c(rval, 54)
-            else if (length(grep("^D$",    letter, ignore.case=TRUE))>0) rval <- c(rval, 52)
-            else if (length(grep("^D\\-$", letter, ignore.case=TRUE))>0) rval <- c(rval, 50)
-            else if (length(grep("^F$",    letter, ignore.case=TRUE))>0) rval <- c(rval, 25)
+            if      (length(grep("^A\\+$", letter, ignore.case=TRUE))>0) rval <- c(rval, 0.5*(90+100))
+            else if (length(grep("^A$",    letter, ignore.case=TRUE))>0) rval <- c(rval, 0.5*(85+90))
+            else if (length(grep("^A\\-$", letter, ignore.case=TRUE))>0) rval <- c(rval, 0.5*(80+85))
+            else if (length(grep("^B\\+$", letter, ignore.case=TRUE))>0) rval <- c(rval, 0.5*(75+80))
+            else if (length(grep("^B$",    letter, ignore.case=TRUE))>0) rval <- c(rval, 0.5*(70+75))
+            else if (length(grep("^B\\-$", letter, ignore.case=TRUE))>0) rval <- c(rval, 0.5*(65+70))
+            else if (length(grep("^C\\+$", letter, ignore.case=TRUE))>0) rval <- c(rval, 0.5*(62+65))
+            else if (length(grep("^C$",    letter, ignore.case=TRUE))>0) rval <- c(rval, 0.5*(58+62))
+            else if (length(grep("^C\\-$", letter, ignore.case=TRUE))>0) rval <- c(rval, 0.5*(55+58))
+            else if (length(grep("^D$",    letter, ignore.case=TRUE))>0) rval <- c(rval, 0.5*(50+55))
+            else if (length(grep("^F$",    letter, ignore.case=TRUE))>0) rval <- c(rval, 0.5*( 0+50))
             else stop("Unknown letter grade '",letter,"'")
         } else {
             if (length(grep("^A\\+$", letter, ignore.case=TRUE))>0) rval <- c(rval, scale*4.3/4.3)
