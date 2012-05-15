b1a7336f06814bce485afa71006984db38ba328c
Dan Kelley <kelley.dan@gmail.com>
2012-05-15 14:14:49 -0300

    DRY on grade mapping

--- R/p2l.R
+++ R/p2l.R
@@ -1,33 +1,10 @@
-p2l <- function(p, scheme="A")
+p2l <- function(p, scheme=c("A", "B"))
 {
-    rval <- NULL
-    np <- length(p)
-    if (scheme == "A") {
-        for (i in 1:np) {
-            if      (p[i] >= 90)	rval<-c(rval,"A+")
-            else if (p[i] >= 85)	rval<-c(rval,"A" )
-            else if (p[i] >= 80)	rval<-c(rval,"A-")
-            else if (p[i] >= 75)	rval<-c(rval,"B+")
-            else if (p[i] >= 70)	rval<-c(rval,"B" )
-            else if (p[i] >= 65)	rval<-c(rval,"B-")
-            else if (p[i] >= 62)	rval<-c(rval,"C+")
-            else if (p[i] >= 58)	rval<-c(rval,"C" )
-            else if (p[i] >= 55)	rval<-c(rval,"C-")
-            else if (p[i] >= 50)	rval<-c(rval,"D")
-            else                    rval<-c(rval,"F" )
-        }
-    } else if (scheme == "B") {
-        for (i in 1:np) {
-            if      (p[i] >= 90)	rval<-c(rval,"A+")
-            else if (p[i] >= 85)	rval<-c(rval,"A" )
-            else if (p[i] >= 80)	rval<-c(rval,"A-")
-            else if (p[i] >= 75)	rval<-c(rval,"B+")
-            else if (p[i] >= 70)	rval<-c(rval,"B" )
-            else if (p[i] >= 65)	rval<-c(rval,"B-")
-            else                        rval<-c(rval,"F" )
-        }
-    } else {
-        stop("Unknown scheme", scheme)
-    }
-    rval;
+    scheme <- match.arg(scheme)
+    n <- length(p)
+    g <- gradeMapping(scheme)
+    rval <- vector("character", n)
+    for (i in 1:n)
+        rval[i] <- g$letter[sum(p[i]>=g$lowerLimit)]
+    rval
 }
